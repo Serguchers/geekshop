7b15e5190ad267e35f1aa55d21f939f53334f082
Kanokhovich Sergei <81819528+Serguchers@users.noreply.github.com>
2021-11-04 22:30:53 +0200

    Lesson_2. Templates
    * Lesson 2. Templates
    
    * Lesson 2. Context
    
    * Lesson 2. Template Context
    
    * Lesson 2. Context with json

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,11 +1,31 @@
+import datetime
+import json
+
 from django.shortcuts import render
 
-# Create your views here.
+with open('context_data/data.json', 'r') as f:
+    context_data = json.load(f)
+
+main_data = context_data['main']
+products_data = context_data['products']
+contact_data = context_data['contact']
+
+
 def main(request):
-    return render(request, 'mainapp/index.html')
+
+    content = {"title": main_data['title'], "products": main_data['products']}
+    return render(request, "mainapp/index.html", content)
+
 
 def products(request):
-    return render(request, 'mainapp/products.html')
+
+    content = {"title": products_data['title'], "links_menu": products_data['links_menu'], "same_products": products_data['same_products']}
+    return render(request, "mainapp/products.html", content)
+
 
 def contact(request):
-    return render(request, 'mainapp/contact.html')
+
+    visit_date = datetime.datetime.now()
+
+    content = {"title": contact_data['title'], "visit_date": visit_date, "locations": contact_data['locations']}
+    return render(request, "mainapp/contact.html", content)
