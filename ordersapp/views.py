53c12c08d9356c54ed586cc0517f458761793b2c
django@194-67-105-12.cloudvps.regruhosting.ru <django@194-67-105-12.cloudvps.regruhosting.ru>
2021-12-20 15:27:04 +0300

    lesson_15. Changes from lesson_14 and code style fix

--- ordersapp/views.py
+++ ordersapp/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.db import transaction
 from django.db.models.signals import pre_delete, pre_save
 from django.dispatch import receiver
@@ -6,7 +7,7 @@ from django.shortcuts import HttpResponseRedirect, get_object_or_404
 from django.urls import reverse, reverse_lazy
 from django.views.generic import CreateView, DeleteView, ListView, UpdateView
 from django.views.generic.detail import DetailView
-from django.contrib.auth.mixins import LoginRequiredMixin
+
 from basketapp.models import Basket
 from ordersapp.forms import OrderItemForm
 from ordersapp.models import Order, OrderItem
@@ -142,8 +143,10 @@ def product_quantity_update_delete(instance, **kwargs):
     instance.product.quantity += instance.quantity
     instance.product.save()
 
-from mainapp.models import Product
 from django.http import JsonResponse
+
+from mainapp.models import Product
+
 
 def get_product_price(request, pk):
     if request.is_ajax():
